692fc0f402c23b963e41b80ebf00760a887f637d
ProfiRecht <mike.fell1997@outlook.de>
2025-03-20 22:38:35 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import QuizApp from "./QuizApp";
+import QuizApp from "../QuizApp";
 
-ReactDOM.render(<QuizApp />, document.getElementById("root"));
+export default function Home() {
+  return <QuizApp />;
+}
