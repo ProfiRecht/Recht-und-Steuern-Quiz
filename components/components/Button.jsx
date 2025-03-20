a689fbf6e2592c77b40f0910ea23217df94c5074
ProfiRecht <mike.fell1997@outlook.de>
2025-03-20 23:32:19 +0100

    Update Button.jsx

--- components/components/Button.jsx
+++ components/components/Button.jsx
@@ -1,1 +1,11 @@
+export function Button({ children, onClick }) {
+  return (
+    <button
+      className="p-3 bg-blue-600 rounded-lg hover:bg-blue-700 transition"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
 
